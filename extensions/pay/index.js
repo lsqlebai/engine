91e741db67479a5391a55392eee92d5fe8ca9112
乐百 <lsqlebai@163.com>
2017-02-08 11:31:10 +0800

    add paycall for the js

--- extensions/pay/index.js
+++ extensions/pay/index.js
@@ -3,8 +3,9 @@
  */
 var cls = "com/iflytek/unipay/js/UniPay";
 PayComponent = {
-    pay: function (order, payMode) {
+    pay: function (order, payMode, callback) {
         if (cc.sys.os == cc.sys.OS_ANDROID) {
+            PayCallback.addListener(callback);
             jsb.reflection.callStaticMethod(cls, "pay", "(Ljava/lang/String;Ljava/lang/String;)V", order, payMode);
             return true;
         } else {
@@ -12,8 +13,9 @@ PayComponent = {
         }
     },
 
-    payMonth: function (order, payMode) {
+    payMonth: function (order, payMode, callback) {
         if (cc.sys.os == cc.sys.OS_ANDROID) {
+            PayCallback.addListener(callback);
             jsb.reflection.callStaticMethod(cls, "payMonth", "(Ljava/lang/String;Ljava/lang/String;)V", order, payMode);
             return true;
         } else {
@@ -21,6 +23,23 @@ PayComponent = {
         }
     }
 };
+
+if (cc.sys.os == cc.sys.OS_ANDROID) {
+    PayCallback = {
+        listeners: [],
+        addListener: function (listener) {
+            this.listeners.push(listener);
+        },
+
+        onPay: function (payState) {
+            for (var i = 0; i < this.listeners.length; i++) {
+                this.listeners[i](payState);
+            }
+            this.listeners.splice(0, this.listeners.length);
+        }
+    };
+    cc.PayListener.addListener(PayCallback, "onPay");
+}
 
 
 module.exports = PayComponent;
