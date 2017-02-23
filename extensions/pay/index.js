6aa58230c400cb170a5e40f8dfb01abca4ac4cbc
乐百 <lsqlebai@163.com>
2017-02-23 14:30:55 +0800

    add auth api for pay

--- extensions/pay/index.js
+++ extensions/pay/index.js
@@ -5,7 +5,7 @@ var cls = "com/iflytek/unipay/js/UniPay";
 PayComponent = {
     pay: function (order, payMode, callback) {
         if (cc.sys.os == cc.sys.OS_ANDROID) {
-            PayCallback.addListener(callback);
+            PayCallback.addListener("Pay", callback);
             jsb.reflection.callStaticMethod(cls, "pay", "(Ljava/lang/String;Ljava/lang/String;)V", order, payMode);
             return true;
         } else {
@@ -15,8 +15,17 @@ PayComponent = {
 
     payMonth: function (order, payMode, callback) {
         if (cc.sys.os == cc.sys.OS_ANDROID) {
-            PayCallback.addListener(callback);
+            PayCallback.addListener("Pay", callback);
             jsb.reflection.callStaticMethod(cls, "payMonth", "(Ljava/lang/String;Ljava/lang/String;)V", order, payMode);
+            return true;
+        } else {
+            return false;
+        }
+    },
+    checkAuth: function (productId, callback) {
+        if (cc.sys.os == cc.sys.OS_ANDROID) {
+            PayCallback.addListener("Auth", callback);
+            jsb.reflection.callStaticMethod(cls, "checkAuth", "(Ljava/lang/String;)V", productId);
             return true;
         } else {
             return false;
@@ -26,19 +35,34 @@ PayComponent = {
 
 if (cc.sys.os == cc.sys.OS_ANDROID) {
     PayCallback = {
-        listeners: [],
-        addListener: function (listener) {
-            this.listeners.push(listener);
+        payListeners: [],
+        authListeners: [],
+        addListener: function (type, listener) {
+            switch (type) {
+                case "Pay":
+                    this.payListeners.push(listener);
+                    break;
+                case "Auth":
+                    this.authListeners.push(listener);
+                    break;
+            }
         },
 
         onPay: function (payState) {
-            for (var i = 0; i < this.listeners.length; i++) {
-                this.listeners[i](payState);
+            for (var i = 0; i < this.payListeners.length; i++) {
+                this.payListeners[i](payState);
             }
-            this.listeners.splice(0, this.listeners.length);
+            this.payListeners.splice(0, this.payListeners.length);
+        },
+        onAuth: function (result) {
+            for (var i = 0; i < this.authListeners.length; i++) {
+                this.authListeners[i](result);
+            }
+            this.authListeners.splice(0, this.authListeners.length);
         }
     };
-    cc.PayListener.addListener(PayCallback, "onPay");
+    cc.PayListener.addListener("Pay", PayCallback, "onPay");
+    cc.PayListener.addListener("Auth", PayCallback, "onAuth");
 }
 
 
